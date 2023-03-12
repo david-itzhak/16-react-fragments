4a54a80525216fcca15d0ca009554eadbb065f7c
david-itzhak <daviditzhak7@gmail.com>
2023-03-12 11:34:26 +0200

    Initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <>
+            <h1>Pupar</h1>
+            <h3>Drupar</h3>
+            <button>Like!</button>
+        </>
+
+);
+}
+export default Card;
